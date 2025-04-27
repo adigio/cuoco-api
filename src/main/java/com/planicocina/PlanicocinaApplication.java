aaf864e0e8a86a84678a5711124ca67cd2e351f6
Alan Di Giovanni <alan.digiovanni@redb.ee>
2025-04-27 15:23:33 -0300

    feat(PC-9): Added spring base project

---
+++ src/main/java/com/planicocina/PlanicocinaApplication.java
@@ -0,0 +1,13 @@
+package com.planicocina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanicocinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanicocinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planicocina/PlanicocinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planicocina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanicocinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
