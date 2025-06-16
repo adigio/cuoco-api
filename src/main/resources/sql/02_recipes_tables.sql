26be45747791db70a3329f042e55f6b28d3fbefc
Alan Di Giovanni <alan.digiovanni@redb.ee>
2025-06-16 04:59:00 -0300

    feat(PC-115): Inserts in category and unit tables

--- src/main/resources/sql/02_recipes_tables.sql
+++ src/main/resources/sql/02_recipes_tables.sql
@@ -67,7 +67,51 @@ CREATE TABLE `user_recipes`
     CONSTRAINT `FK_user_recipes_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
+INSERT INTO category(id, description)
+VALUES (1, 'Verduras'),
+       (2, 'Frutas'),
+       (3, 'Carnes'),
+       (4, 'Pescados y mariscos'),
+       (5, 'Lácteos'),
+       (6, 'Huevos'),
+       (7, 'Cereales y granos'),
+       (8, 'Legumbres'),
+       (9, 'Aceites y grasas'),
+       (10, 'Especias y condimentos'),
+       (11, 'Hierbas frescas'),
+       (12, 'Bebidas'),
+       (13, 'Panadería y pastelería'),
+       (14, 'Frutos secos y semillas'),
+       (15, 'Congelados'),
+       (16, 'Alimentos en conserva'),
+       (17, 'Salsas y aderezos'),
+       (18, 'Snacks y golosinas'),
+       (19, 'Productos veganos'),
+       (20, 'Otros');
 
+INSERT INTO unit (id, description, symbol)
+VALUES (1, 'Mililitro', 'ml'),
+       (2, 'Gramo', 'gr'),
+       (3, 'Kilogramo', 'kg'),
+       (4, 'Litro', 'l'),
+       (5, 'Cucharada', 'cda'),
+       (6, 'Cucharadita', 'cdta'),
+       (7, 'Unidad', 'ud'),
+       (8, 'Taza', 'tz'),
+       (9, 'Pizca', ''),
+       (10, 'Diente', ''),
+       (11, 'Lata', ''),
+       (12, 'Botella', ''),
+       (13, 'Sobre', ''),
+       (14, 'Rodaja', ''),
+       (15, 'Rebanada', ''),
+       (16, 'Puñado', ''),
+       (17, 'Onza', 'oz'),
+       (18, 'Libra', 'lb'),
+       (19, 'Miligramo', 'mg'),
+       (20, 'Centilitro', 'cl'),
+       (21, 'Copa', ''),
+       (22, 'Cucharón', '');
 
 
 
