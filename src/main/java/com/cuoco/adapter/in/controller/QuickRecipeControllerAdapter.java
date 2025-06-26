38fc510fb91e24bd6c141f1eb74956aecac5c9a6
Alan Di Giovanni <alan.digiovanni@redb.ee>
2025-06-26 16:56:25 -0300

    feat(PC-134): Some fixes after release merge

--- src/main/java/com/cuoco/adapter/in/controller/QuickRecipeControllerAdapter.java
+++ src/main/java/com/cuoco/adapter/in/controller/QuickRecipeControllerAdapter.java
@@ -6,7 +6,13 @@ import com.cuoco.adapter.in.controller.model.QuickRecipeRequest;
 import com.cuoco.adapter.in.controller.model.RecipeResponse;
 import com.cuoco.adapter.in.controller.model.UnitResponse;
 import com.cuoco.application.port.in.FindOrGenerateRecipeCommand;
+import com.cuoco.application.usecase.model.Allergy;
+import com.cuoco.application.usecase.model.CookLevel;
+import com.cuoco.application.usecase.model.Diet;
+import com.cuoco.application.usecase.model.DietaryNeed;
 import com.cuoco.application.usecase.model.Ingredient;
+import com.cuoco.application.usecase.model.MealType;
+import com.cuoco.application.usecase.model.PreparationTime;
 import com.cuoco.application.usecase.model.Recipe;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -50,20 +56,17 @@ public class QuickRecipeControllerAdapter {
         return RecipeResponse.builder()
                 .id(recipe.getId())
                 .name(recipe.getName())
-                .image(recipe.getImage())
                 .subtitle(recipe.getSubtitle())
                 .description(recipe.getDescription())
-                .preparationTime(recipe.getPreparationTime())
                 .instructions(recipe.getInstructions())
-                .ingredients(
-                        recipe.getIngredients().stream().map(this::buildIngredientResponse).toList()
-                )
-                .cookLevel(
-                        ParametricResponse.builder()
-                                .id(recipe.getCookLevel().getId())
-                                .description(recipe.getCookLevel().getDescription())
-                                .build()
-                )
+                .image(recipe.getImage())
+                .preparationTime(buildParametricResponse(recipe.getPreparationTime()))
+                .cookLevel(buildParametricResponse(recipe.getCookLevel()))
+                .diet(buildParametricResponse(recipe.getDiet()))
+                .mealTypes(recipe.getMealTypes().stream().map(this::buildParametricResponse).toList())
+                .allergies(recipe.getAllergies().stream().map(this::buildParametricResponse).toList())
+                .dietaryNeeds(recipe.getDietaryNeeds().stream().map(this::buildParametricResponse).toList())
+                .ingredients(recipe.getIngredients().stream().map(this::buildIngredientResponse).toList())
                 .build();
     }
 
@@ -77,6 +80,48 @@ public class QuickRecipeControllerAdapter {
                         .symbol(ingredient.getUnit().getSymbol())
                         .build()
                 )
+                .build();
+    }
+
+    private ParametricResponse buildParametricResponse(PreparationTime preparationTime) {
+        return ParametricResponse.builder()
+                .id(preparationTime.getId())
+                .description(preparationTime.getDescription())
+                .build();
+    }
+
+    private ParametricResponse buildParametricResponse(CookLevel cookLevel) {
+        return ParametricResponse.builder()
+                .id(cookLevel.getId())
+                .description(cookLevel.getDescription())
+                .build();
+    }
+
+    private ParametricResponse buildParametricResponse(Diet diet) {
+        return ParametricResponse.builder()
+                .id(diet.getId())
+                .description(diet.getDescription())
+                .build();
+    }
+
+    private ParametricResponse buildParametricResponse(MealType mealType) {
+        return ParametricResponse.builder()
+                .id(mealType.getId())
+                .description(mealType.getDescription())
+                .build();
+    }
+
+    private ParametricResponse buildParametricResponse(Allergy allergy) {
+        return ParametricResponse.builder()
+                .id(allergy.getId())
+                .description(allergy.getDescription())
+                .build();
+    }
+
+    private ParametricResponse buildParametricResponse(DietaryNeed dietaryNeed) {
+        return ParametricResponse.builder()
+                .id(dietaryNeed.getId())
+                .description(dietaryNeed.getDescription())
                 .build();
     }
 } 
