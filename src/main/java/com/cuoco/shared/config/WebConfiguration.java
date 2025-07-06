06ad59796c1d20b3e7bcdaf2057d2981d2e09cf8
Alan Di Giovanni <alan.digiovanni@redb.ee>
2025-07-06 13:11:49 -0300

    fix: [WebConfig] Added PATCH in cors configuration

--- src/main/java/com/cuoco/shared/config/WebConfiguration.java
+++ src/main/java/com/cuoco/shared/config/WebConfiguration.java
@@ -1,7 +1,9 @@
-package com.cuoco.shared.config.security;
+package com.cuoco.shared.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,20 +15,27 @@ public class WebConfiguration {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:3000",
                                 "https://dev.cuoco.com.ar",
                                 "https://www.cuoco.com.ar"
                         )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.PATCH.name(),
+                                HttpMethod.DELETE.name(),
+                                HttpMethod.OPTIONS.name()
+                        )
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
 
             @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            public void addResourceHandlers(@NotNull ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("/api/images/**")
                         .addResourceLocations("classpath:/imagenes/");
             }
